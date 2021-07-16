0c603f204509272c987092a724d1e6ae99d41779
Cat <morancathy@gmail.com>
2021-07-16 00:06:08 -0500

    crazy object complete

--- script.js
+++ script.js
@@ -15,3 +15,48 @@ const cathy = {
 
 // 4)
 console.log(`${cathy.hobby} \n`);
+
+// Crazy object
+const crazyObject = {
+  taco: [{meat: 'steak',
+          cheese: ['panela', 'queso', 'chihuahua']},
+         {meat: 'chicken',
+          salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]},
+        ],
+  larry: {
+    nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
+    quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
+    characters: [
+      {
+      name: "Jeff",
+      occupation: "manager"
+      },
+      {
+      name: "funkhauser",
+      occupation: "tv dude"
+      },
+      {
+      name: "susie",
+      occupation: "jeffs wife",
+      favourtieHobby: "Swearing at Larry and Jeff"
+      },
+    ]
+  }
+}
+
+// 1)
+console.log(`${crazyObject.taco[1].salsa[5]} \n`)
+// 2)
+console.log(`${crazyObject.larry.quotes[0]} \n`)
+// 3)
+console.log(`${crazyObject.larry.characters[2].favourtieHobby} \n`)
+// 4)
+console.log(`${crazyObject.larry.nicknames[1]} \n`)
+// 5)
+console.log(crazyObject.larry.characters[1])
+console.log(`\n`)
+// 6)
+const string = "I'm trying to elevate small talk to medium talk";
+crazyObject.larry.quotes.push(string);
+console.log(crazyObject.larry.quotes)
+console.log(`\n`)
