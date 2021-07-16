faa0b0e4277db1d09e6a1244a043701241992c3e
Cat <morancathy@gmail.com>
2021-07-16 00:04:54 -0500

    took it easy

---
+++ script.js
@@ -0,0 +1,17 @@
+//Take it Easy
+// 1)
+const rainbowArray = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+
+// 2)
+console.log(`${rainbowArray[4]} \n`);
+
+// 3)
+const cathy = {
+  food: 'caramel',
+  hobby: 'diving',
+  town: 'Chi-town!',
+  favDatatype: 'object'
+};
+
+// 4)
+console.log(`${cathy.hobby} \n`);
