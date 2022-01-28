4bcfd96d19c017a016aa236dce017b770848e3a7
Cat <morancathy@gmail.com>
2022-01-28 11:02:28 -0600

    update finding oddFilms to function and loop

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 //Take it Easy
 // 1)
-const rainbowArray = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+const rainbowArray = [
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "indigo",
+  "violet",
+];
 
 // 2)
 console.log(`${rainbowArray[4]} \n`);
 
 // 3)
 const cathy = {
-  food: 'caramel',
-  hobby: 'diving',
-  town: 'Chi-town!',
-  favDatatype: 'object'
+  food: "caramel",
+  hobby: "diving",
+  town: "Chi-town!",
+  favDatatype: "object",
 };
 
 // 4)
@@ -18,124 +26,259 @@ console.log(`${cathy.hobby} \n`);
 
 // Crazy object
 const crazyObject = {
-  taco: [{meat: 'steak',
-          cheese: ['panela', 'queso', 'chihuahua']},
-         {meat: 'chicken',
-          salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]},
-        ],
+  taco: [
+    { meat: "steak", cheese: ["panela", "queso", "chihuahua"] },
+    {
+      meat: "chicken",
+      salsa: [
+        "pico",
+        "hot",
+        "hotter",
+        "really hot",
+        "really really hot",
+        "omg my mouth is burning",
+      ],
+    },
+  ],
   larry: {
     nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
-    quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
+    quotes: [
+      "Pretty pretty prettayyyyy good",
+      "Is that a parkinson's thing?",
+      "women love a self confident bald man",
+      "I'm a walking talking enigma",
+    ],
     characters: [
       {
-      name: "Jeff",
-      occupation: "manager"
+        name: "Jeff",
+        occupation: "manager",
       },
       {
-      name: "funkhauser",
-      occupation: "tv dude"
+        name: "funkhauser",
+        occupation: "tv dude",
       },
       {
-      name: "susie",
-      occupation: "jeffs wife",
-      favourtieHobby: "Swearing at Larry and Jeff" },
-    ]
-  }
-}
+        name: "susie",
+        occupation: "jeffs wife",
+        favourtieHobby: "Swearing at Larry and Jeff",
+      },
+    ],
+  },
+};
 
 // 1)
-console.log(`${crazyObject.taco[1].salsa[5]} \n`)
+console.log(`${crazyObject.taco[1].salsa[5]} \n`);
 // 2)
-console.log(`${crazyObject.larry.quotes[0]} \n`)
+console.log(`${crazyObject.larry.quotes[0]} \n`);
 // 3)
-console.log(`${crazyObject.larry.characters[2].favourtieHobby} \n`)
+console.log(`${crazyObject.larry.characters[2].favourtieHobby} \n`);
 // 4)
-console.log(`${crazyObject.larry.nicknames[1]} \n`)
+console.log(`${crazyObject.larry.nicknames[1]} \n`);
 // 5)
-console.log(crazyObject.larry.characters[1])
-console.log(`\n`)
+console.log(crazyObject.larry.characters[1]);
+console.log(`\n`);
 // 6)
 const string = "I'm trying to elevate small talk to medium talk";
 crazyObject.larry.quotes.push(string);
-console.log(crazyObject.larry.quotes)
-console.log(`\n`)
+console.log(crazyObject.larry.quotes);
+console.log(`\n`);
 
 // Object-ception
 const inception = {
-   reality: {
-       dreamLayer1: {
-           dreamLayer2: {
-               dreamLayer3: {
-                   dreamLayer4: {
-                       dreamLayer5: {
-                           dreamLayer6: {
-                               limbo: "Joseph Gordon Levitt"
-                          }
-                       }
-                   }
-               }
-           }
-       }
-   }
-}
+  reality: {
+    dreamLayer1: {
+      dreamLayer2: {
+        dreamLayer3: {
+          dreamLayer4: {
+            dreamLayer5: {
+              dreamLayer6: {
+                limbo: "Joseph Gordon Levitt",
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+};
 
-inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo = null;
+inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo =
+  null;
 
-console.log(`${inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo} \n`)
+console.log(
+  `${inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo} \n`
+);
 
 // Bond Films
 const bondFilms = [
-  { "title" : "Skyfall", "year" : 2012, "actor" : "Daniel Craig", "gross" : "$1,108,561,008" },
-  { "title" : "Thunderball", "year" : 1965, "actor" : "Sean Connery", "gross" : "$1,014,941,117" },
-  { "title" : "Goldfinger", "year" : 1964, "actor" : "Sean Connery", "gross" : "$912,257,512" },
-  { "title" : "Live and Let Die", "year" : 1973, "actor" : "Roger Moore", "gross" : "$825,110,761" },
-  { "title" : "You Only Live Twice", "year" : 1967, "actor" : "Sean Connery", "gross" : "$756,544,419" },
-  { "title" : "The Spy Who Loved Me", "year" : 1977, "actor" : "Roger Moore", "gross" : "$692,713,752" },
-  { "title" : "Casino Royale", "year" : 2006, "actor" : "Daniel Craig", "gross" : "$669,789,482" },
-  { "title" : "Moonraker", "year" : 1979, "actor" : "Roger Moore", "gross" : "$655,872,400" },
-  { "title" : "Diamonds Are Forever", "year" : 1971, "actor" : "Sean Connery", "gross" : "$648,514,469" },
-  { "title" : "Quantum of Solace", "year" : 2008, "actor" : "Daniel Craig", "gross" : "$622,246,378" },
-  { "title" : "From Russia with Love", "year" : 1963, "actor" : "Sean Connery", "gross" : "$576,277,964" },
-  { "title" : "Die Another Day", "year" : 2002, "actor" : "Pierce Brosnan", "gross" : "$543,639,638" },
-  { "title" : "Goldeneye", "year" : 1995, "actor" : "Pierce Brosnan", "gross" : "$529,548,711" },
-  { "title" : "On Her Majesty's Secret Service", "year" : 1969, "actor" : "George Lazenby", "gross" : "$505,899,782" },
-  { "title" : "The World is Not Enough", "year" : 1999, "actor" : "Pierce Brosnan", "gross" : "$491,617,153" },
-  { "title" : "For Your Eyes Only", "year" : 1981, "actor" : "Roger Moore", "gross" : "$486,468,881" },
-  { "title" : "Tomorrow Never Dies", "year" : 1997, "actor" : "Pierce Brosnan", "gross" : "$478,946,402" },
-  { "title" : "The Man with the Golden Gun", "year" : 1974, "actor" : "Roger Moore", "gross" : "$448,249,281" },
-  { "title" : "Dr. No", "year" : 1962, "actor" : "Sean Connery", "gross" : "$440,759,072" },
-  { "title" : "Octopussy", "year" : 1983, "actor" : "Roger Moore", "gross" : "$426,244,352" },
-  { "title" : "The Living Daylights", "year" : 1987, "actor" : "Timothy Dalton", "gross" : "$381,088,866" },
-  { "title" : "A View to a Kill", "year" : 1985, "actor" : "Roger Moore", "gross" : "$321,172,633" },
-  { "title" : "License to Kill", "year" : 1989, "actor" : "Timothy Dalton", "gross" : "$285,157,191" }
+  {
+    title: "Skyfall",
+    year: 2012,
+    actor: "Daniel Craig",
+    gross: "$1,108,561,008",
+  },
+  {
+    title: "Thunderball",
+    year: 1965,
+    actor: "Sean Connery",
+    gross: "$1,014,941,117",
+  },
+  {
+    title: "Goldfinger",
+    year: 1964,
+    actor: "Sean Connery",
+    gross: "$912,257,512",
+  },
+  {
+    title: "Live and Let Die",
+    year: 1973,
+    actor: "Roger Moore",
+    gross: "$825,110,761",
+  },
+  {
+    title: "You Only Live Twice",
+    year: 1967,
+    actor: "Sean Connery",
+    gross: "$756,544,419",
+  },
+  {
+    title: "The Spy Who Loved Me",
+    year: 1977,
+    actor: "Roger Moore",
+    gross: "$692,713,752",
+  },
+  {
+    title: "Casino Royale",
+    year: 2006,
+    actor: "Daniel Craig",
+    gross: "$669,789,482",
+  },
+  {
+    title: "Moonraker",
+    year: 1979,
+    actor: "Roger Moore",
+    gross: "$655,872,400",
+  },
+  {
+    title: "Diamonds Are Forever",
+    year: 1971,
+    actor: "Sean Connery",
+    gross: "$648,514,469",
+  },
+  {
+    title: "Quantum of Solace",
+    year: 2008,
+    actor: "Daniel Craig",
+    gross: "$622,246,378",
+  },
+  {
+    title: "From Russia with Love",
+    year: 1963,
+    actor: "Sean Connery",
+    gross: "$576,277,964",
+  },
+  {
+    title: "Die Another Day",
+    year: 2002,
+    actor: "Pierce Brosnan",
+    gross: "$543,639,638",
+  },
+  {
+    title: "Goldeneye",
+    year: 1995,
+    actor: "Pierce Brosnan",
+    gross: "$529,548,711",
+  },
+  {
+    title: "On Her Majesty's Secret Service",
+    year: 1969,
+    actor: "George Lazenby",
+    gross: "$505,899,782",
+  },
+  {
+    title: "The World is Not Enough",
+    year: 1999,
+    actor: "Pierce Brosnan",
+    gross: "$491,617,153",
+  },
+  {
+    title: "For Your Eyes Only",
+    year: 1981,
+    actor: "Roger Moore",
+    gross: "$486,468,881",
+  },
+  {
+    title: "Tomorrow Never Dies",
+    year: 1997,
+    actor: "Pierce Brosnan",
+    gross: "$478,946,402",
+  },
+  {
+    title: "The Man with the Golden Gun",
+    year: 1974,
+    actor: "Roger Moore",
+    gross: "$448,249,281",
+  },
+  { title: "Dr. No", year: 1962, actor: "Sean Connery", gross: "$440,759,072" },
+  {
+    title: "Octopussy",
+    year: 1983,
+    actor: "Roger Moore",
+    gross: "$426,244,352",
+  },
+  {
+    title: "The Living Daylights",
+    year: 1987,
+    actor: "Timothy Dalton",
+    gross: "$381,088,866",
+  },
+  {
+    title: "A View to a Kill",
+    year: 1985,
+    actor: "Roger Moore",
+    gross: "$321,172,633",
+  },
+  {
+    title: "License to Kill",
+    year: 1989,
+    actor: "Timothy Dalton",
+    gross: "$285,157,191",
+  },
 ];
 
 // 1)
 function listOfBondTitles() {
-    let bondTitles = [];
-    //either for loop works:
-    // for(let i = 0; i < bondFilms.length; i ++) {
-    //   bondTitles.push(bondFilms[i].title)
-    // }
-    for(let film of bondFilms) {
-        bondTitles.push(film.title)
-    }
-    console.log("List of Bond Titles\n", bondTitles)
+  let bondTitles = [];
+  //either for loop works:
+  // for(let i = 0; i < bondFilms.length; i ++) {
+  //   bondTitles.push(bondFilms[i].title)
+  // }
+  for (let film of bondFilms) {
+    bondTitles.push(film.title);
+  }
+  console.log("List of Bond Titles\n", bondTitles);
 }
 listOfBondTitles();
 
 // 2)
-let oddBonds = [];
-for(let i = 0; i < bondFilms.length; i ++) {
-  if(bondFilms[i].year %2 !== 0){
-    oddBonds.push(bondFilms[i])
+function oddBondTitles() {
+  let oddBonds = [];
+  // for (let i = 0; i < bondFilms.length; i++) {
+  //   if (bondFilms[i].year % 2 !== 0) {
+  //     oddBonds.push(bondFilms[i]);
+  //   }
+  for (let film of bondFilms) {
+    if(film.year % 2 !== 0) {
+        oddBonds.push(film)
+    }
   }
-};
-console.log(oddBonds)
+  console.log("List of Odd Bond Titles\n", oddBonds);
+}
+oddBondTitles();
 
 // 3)
 let gross = 0;
-for(let i = 0; i < bondFilms.length; i ++) {
-  gross = gross + parseInt(bondFilms[i].gross.replace(/\$|,/g, ''))
-};
-console.log('$',gross)
+for (let i = 0; i < bondFilms.length; i++) {
+  gross = gross + parseInt(bondFilms[i].gross.replace(/\$|,/g, ""));
+}
+console.log("$", gross);
