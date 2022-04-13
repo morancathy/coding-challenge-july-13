62f050c2617e38f10a93cc97ecc50d5effea0e8d
Cat <morancathy@gmail.com>
2022-04-12 23:11:21 -0500

    math functions

---
+++ math/squared-divideBy3.js
@@ -0,0 +1,15 @@
+// Squared Number
+const square = (num) => {
+  let squaredNumber = Math.pow(num,2);
+  console.log("Squared Number: ", squaredNumber)
+}
+square(5)
+//########################################################################
+
+// Divide 3 Numbers
+const divideThreeNums = (num1, num2, num3) => {
+  let numA = num2/num1;
+  let numB = num3/numA;
+  console.log("Divide Three Numbers: ", numB)
+}
+divideThreeNums(10, 5, 2)
