4dbca35792aba240fc57298d004047a6f64d8f92
Cat <morancathy@gmail.com>
2022-04-13 23:12:32 -0500

    military time part 2

--- convertMilitaryTime.js
+++ convertMilitaryTime.js
@@ -19,3 +19,28 @@ function timeConversion(s) {
 }
 
 timeConversion("01:05:45PM");
+///////////////////////////////////////////////////////////////////////
+//military time part 2
+
+function timeConversion1(s) {
+  let array = s.split(":"); // convert string to array, seperated by :
+  let hour = parseInt(array[0]); // change hour from string to integer
+
+  if (s.substring(8) == "PM" && hour != 12) {
+    // convert to military time
+    hour = hour + 12;
+  } else if (s.substring(8) == "AM" && hour == 12) {
+    hour = "00";
+  } else if (s.substring(8) == "PM" && hour == 12) {
+    hour = hour;
+  } else {
+    hour = `0${hour}`;
+  }
+  hour = hour.toString(); // change hour from integer back to string
+  array.splice(0, 1, hour); // replace old hour with military hour
+  let militaryTime = array.join(":"); // convert array back to string
+  militaryTime = militaryTime.substring(0, 8); // get ride of "PM"
+  return militaryTime;
+}
+
+console.log(timeConversion1("12:05:45PM"));
